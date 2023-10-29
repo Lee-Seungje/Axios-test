e6472a49aa464df9dec0b529f82f5767c6feeb0c
Lee-Seungje <moondgod@gmail.com>
2023-10-29 10:50:09 +0900

    Rename getMeal

--- axios-test/src/api.js
+++ axios-test/src/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const getMeals = async () => {
+const getMeal = async () => {
   const { data } = await axios.get(
     `https://open.neis.go.kr/hub/mealServiceDietInfo?KEY=${process.env.REACT_APP_TOKEN}&Type=json&ATPT_OFCDC_SC_CODE=F10&SD_SCHUL_CODE=7380292&MLSV_YMD=20231030`
   );
