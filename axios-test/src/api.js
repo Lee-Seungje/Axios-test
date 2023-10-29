d5f6440f5cb030d73aec46bd2e563b46b035b813
Lee-Seungje <moondgod@gmail.com>
2023-10-29 10:49:56 +0900

    Update getMeal

--- axios-test/src/api.js
+++ axios-test/src/api.js
@@ -1,13 +1,16 @@
 import axios from "axios";
 
-const ApiTest = () => {
-  const {
-    data: { mealServiceDietInfo },
-  } = axios.get(
-    `https://open.neis.go.kr/hub/mealServiceDietInfo?KEY=${process.env.REACT_APP_TOKEN}&Type=json&pIndex=1&pSize=1&ATPT_OFCDC_SC_CODE=F10&SD_SCHUL_CODE=7380292&MLSV_YMD-2023-10-30`
+const getMeals = async () => {
+  const { data } = await axios.get(
+    `https://open.neis.go.kr/hub/mealServiceDietInfo?KEY=${process.env.REACT_APP_TOKEN}&Type=json&ATPT_OFCDC_SC_CODE=F10&SD_SCHUL_CODE=7380292&MLSV_YMD=20231030`
   );
+  return data.mealServiceDietInfo[1].row[0];
+};
 
-  console.log(mealServiceDietInfo);
+const ApiTest = () => {
+  const data = getMeals();
+
+  console.log(data);
   return <div></div>;
 };
 
