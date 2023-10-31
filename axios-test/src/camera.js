49333bffc9cfc53319d90a2221817383f92568be
Lee-Seungje <moondgod@gmail.com>
2023-10-31 23:37:57 +0900

    Add getImageUrl

--- axios-test/src/camera.js
+++ axios-test/src/camera.js
@@ -1,29 +1,34 @@
 import React, { useRef, useCallback } from "react";
 import Webcam from "react-webcam";
+import { useState } from "react";
 
 const CameraCapture = () => {
-  function dataURItoBlob(dataURI) {
+  const [image, setImage] = useState(null);
+
+  const dataURItoBlob = (dataURI) => {
     const byteString = atob(dataURI.split(",")[1]);
     const ab = new ArrayBuffer(byteString.length);
     const ia = new Uint8Array(ab);
-    for (let i = 0; i < byteString.length; i++) {
+    for (let i = 0; i < byteString.length; i++)
       ia[i] = byteString.charCodeAt(i);
-    }
     return new Blob([ab], { type: "image/png" });
-  }
+  };
 
   const webcamRef = useRef(null);
 
   const capture = useCallback(() => {
     const imageSrc = webcamRef.current.getScreenshot();
     const imageBlob = dataURItoBlob(imageSrc);
-    console.log(imageBlob);
+    setImage(imageBlob);
   }, [webcamRef]);
+
+  const getImageUrl = () => URL.createObjectURL(image);
 
   return (
     <div>
       <Webcam audio={false} ref={webcamRef} screenshotFormat="image/png" />
       <button onClick={capture}>사진 찍기</button>
+      {image && <img src={getImageUrl()} />}
     </div>
   );
 };
