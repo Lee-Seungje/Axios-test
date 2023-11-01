ac0e30d1278510cbf4b6dc4a1e7be0949a48e9ec
Lee-Seungje <moondgod@gmail.com>
2023-11-01 16:08:10 +0900

    Update postImage

--- axios-test/src/api/postImage.js
+++ axios-test/src/api/postImage.js
@@ -4,10 +4,10 @@ const url = "https://openapi.naver.com/v1/vision/celebrity";
 
 export const postImage = async (image) => {
   const formData = new FormData();
-  formData.append("image", image, "image.png");
+  formData.append("image", image);
 
   const { data } = await axios.post(url, formData, {
-    Headers: {
+    headers: {
       "Content-Type": "multipart/form-data",
       "X-Naver-Client-Id": process.env.REACT_APP_NAVER_ID,
       "X-Naver-Client-Secret": process.env.REACT_APP_CLIENT_SECRET,
--- axios-test/src/camera.js
+++ axios-test/src/camera.js
@@ -1,6 +1,7 @@
 import React, { useRef, useCallback } from "react";
 import Webcam from "react-webcam";
 import { useState } from "react";
+import { postImage } from "./api/postImage";
 
 const CameraCapture = ({ goNext }) => {
   const [image, setImage] = useState(null);
@@ -11,7 +12,7 @@ const CameraCapture = ({ goNext }) => {
     const ia = new Uint8Array(ab);
     for (let i = 0; i < byteString.length; i++)
       ia[i] = byteString.charCodeAt(i);
-    return new Blob([ab], { type: "image/png" });
+    return new Blob([ab], { type: "image/jpeg" });
   };
 
   const webcamRef = useRef(null);
@@ -26,10 +27,16 @@ const CameraCapture = ({ goNext }) => {
 
   return (
     <div>
-      <Webcam audio={false} ref={webcamRef} screenshotFormat="image/png" />
+      <Webcam audio={false} ref={webcamRef} screenshotFormat="image/jpeg" />
       <button onClick={capture}>사진 찍기</button>
       {image && <img src={getImageUrl()} />}
-      <button onClick={goNext}>다음</button>
+      <button
+        onClick={() => {
+          console.log(postImage(image));
+        }}
+      >
+        다음
+      </button>
     </div>
   );
 };
