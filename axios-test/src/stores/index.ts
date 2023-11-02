fcd3adf1e8df24f8b2eb443e8717bd891d34f0bd
Lee-Seungje <moondgod@gmail.com>
2023-11-02 23:05:45 +0900

    Add mail

--- axios-test/src/stores/index.ts
+++ axios-test/src/stores/index.ts
@@ -1,6 +1,6 @@
 export * from "./useNameState";
 export * from "./useImageUrlState";
-export * from "./useGenderState";
+export * from "./useMailState";
 export * from "./useAgeState";
 export * from "./useJobState";
 export * from "./useContactState";
--- axios-test/src/stores/useGenderState.js
+++
@@ -1,6 +0,0 @@
-import { create } from "zustand";
-
-export const useGenderState = create((set) => ({
-  gender: "",
-  setGender: (value) => set({ gender: value }),
-}));
