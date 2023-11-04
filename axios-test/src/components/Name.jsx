db90ceec9d21323d38b30cfb1a6f762616b58e3b
Lee-Seungje <moondgod@gmail.com>
2023-11-04 19:06:19 +0900

    Update name responsive

--- axios-test/src/components/Name.jsx
+++ axios-test/src/components/Name.jsx
@@ -9,7 +9,7 @@ const Name = ({ goNext }) => {
   return (
     <div className="flex flex-col	items-center relative">
       <input
-        className="ease-in-out duration-200 focus:border-b border-blue-600 w-custom h-20 bg-neutral-900 text-4xl outline-none mt-24 "
+        className="standard:pl-4 ease-in-out duration-200 focus:border-b border-blue-600 w-custom standard:w-screen h-20 bg-neutral-900 text-4xl outline-none mt-24 "
         value={name}
         placeholder="이름을 입력해주세요."
         onChange={(e) => setName(e.target.value)}
